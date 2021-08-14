f170dec0abba884cb994fc0d13758c265c10713f
muhamednasr <56140418+muhamednasr@users.noreply.github.com>
2021-08-14 02:06:03 +0200

    node learning demo

---
+++ app.js
@@ -0,0 +1,12 @@
+//function sayHello(name){
+//    console.log('Hello ' + name);
+//}
+
+// sayHello('Nasr')
+// var hello = function(){
+// console.log('say hello global variable');
+// }
+
+const logger = require('./logger');
+
+logger.log('message');
