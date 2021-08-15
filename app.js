056b7698ba8f8bdc60d1da3dd394fd1dcedcb483
muhamednasr <56140418+muhamednasr@users.noreply.github.com>
2021-08-15 07:00:44 +0200

    learning Node

--- app.js
+++ app.js
@@ -7,6 +7,38 @@
 // console.log('say hello global variable');
 // }
 
-const logger = require('./logger');
+// const logger = require('./logger');
+// logger.log('message');
 
-logger.log('message');
+// OS module
+const os = require('os');
+
+var totalMem = os.totalmem(); 
+var freeMem = os.freemem();
+
+console.log(`Total Memory: ${totalMem}`);
+console.log(`Free Memory: ${freeMem}`);
+
+// HTTP Module
+const http = require('http');
+// const server = http.createServer();
+// server.on('connection', (socket)=>{
+//     console.log('New connection .....!');
+// })
+
+const server = http.createServer((req,res)=>{
+
+     if (req.url == '/'){
+         res.write('Hello world');
+         res.end();
+     }
+
+     if (req.url == '/api/courses'){
+        res.write(JSON.stringify([1,2,3,4]));
+        res.end();
+    }
+
+});
+
+server.listen(3000);
+console.log('This servers listens to port 3000 ...');
